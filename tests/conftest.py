635039d80f0e83d15b8d248ea7383a0d2eb5a096
L. M. Riza Rizky <42672299+zaRizk7@users.noreply.github.com>
2025-06-04 00:00:01 +0100

    repurpose x as a session wide fixture

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+import torch
+from .toy_data import make_spd_matrix
+
+BATCH_SIZE = 5
+N = 4
+
+torch.manual_seed(0)
+
+
+@pytest.fixture(scope="session")
+def x():
+    return torch.stack([make_spd_matrix(N, dtype=torch.float64).requires_grad_() for _ in range(BATCH_SIZE)], dim=0)
--- tests/functions/test_autograd.py
+++ tests/functions/test_autograd.py
@@ -2,21 +2,10 @@ import pytest
 import torch
 from torch.autograd import gradcheck, gradgradcheck
 
-from spdnet.functions.autograd import *
-
-from ..toy_data import make_spd_matrix
-
-torch.manual_seed(0)
-
-
-@pytest.fixture(scope="module")
-def x():
-    """
-    Fixture to create a symmetric positive definite matrix for testing.
-    """
-    batch = 5
-
-    return torch.stack([make_spd_matrix(4, dtype=torch.float64).requires_grad_() for _ in range(batch)], dim=0)
+from spdnet.functions.exponential import SymmetricMatrixExponential
+from spdnet.functions.logarithm import SymmetricMatrixLogarithm
+from spdnet.functions.power import SymmetricMatrixPower
+from spdnet.functions.rectification import SymmetricMatrixRectification
 
 
 def gradcheck_fn(func, x):
@@ -43,17 +32,19 @@ def test_symmetric_matrix_exponential(x):
     gradcheck_fn(SymmetricMatrixExponential.apply, x)
 
 
-def test_symmetric_matrix_power(x):
+@pytest.mark.parametrize("p", [2, -1, 1 / 2])
+def test_symmetric_matrix_power(x, p):
     """
     Test the SymmetricMatrixPower function using gradcheck.
     """
-    for p in [2, -1, 1 / 2]:
-        gradcheck_fn(lambda x: SymmetricMatrixPower.apply(x, p), x)
+    gradcheck_fn(lambda x: SymmetricMatrixPower.apply(x, p), x)
+    p = torch.tensor(p, dtype=torch.float64).requires_grad_()
+    gradcheck_fn(lambda x: SymmetricMatrixPower.apply(x, p), x)
 
 
-def test_symmetric_matrix_rectification(x):
+@pytest.mark.parametrize("eps", [1e-4, 1e-5, 1e-6])
+def test_symmetric_matrix_rectification(x, eps):
     """
     Test the SymmetricMatrixRectification function using gradcheck.
     """
-    for eps in [1e-4, 1e-5, 1e-6]:
-        gradcheck_fn(lambda x: SymmetricMatrixRectification.apply(x, eps), x)
+    gradcheck_fn(lambda x: SymmetricMatrixRectification.apply(x, eps), x)
