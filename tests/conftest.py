d5ca5436c9a7f567009c80aff8a623f94506204b
L. M. Riza Rizky <42672299+zaRizk7@users.noreply.github.com>
2025-06-09 17:06:06 +0100

    add w fixture for testing bimap

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,8 @@ import torch
 
 from .toy_data import make_spd_matrix
 
-BATCH_SIZE = 5
-N = 4
+BATCH_SIZE = 1
+N = 32
 
 torch.manual_seed(0)
 
@@ -12,3 +12,10 @@ torch.manual_seed(0)
 @pytest.fixture(scope="session")
 def x():
     return torch.stack([make_spd_matrix(N, dtype=torch.float64).requires_grad_() for _ in range(BATCH_SIZE)], dim=0)
+
+
+@pytest.fixture(scope="session")
+def w():
+    w = torch.empty(N // 2, N, dtype=torch.float64)
+    torch.nn.init.orthogonal_(w)
+    return w.requires_grad_()
