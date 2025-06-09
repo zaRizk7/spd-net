4cb56d2f7a720da5512d679c1693fa917eaa0629
L. M. Riza Rizky <42672299+zaRizk7@users.noreply.github.com>
2025-06-09 17:06:23 +0100

    add logscale pre-exp testing

--- tests/functions/test_autograd.py
+++ tests/functions/test_autograd.py
@@ -2,17 +2,24 @@ import pytest
 import torch
 from torch.autograd import gradcheck, gradgradcheck
 
-from spdnet.functions import sym_mat_exp, sym_mat_log, sym_mat_pow, sym_mat_rec
+from spdnet.functions import bilinear, sym_mat_exp, sym_mat_log, sym_mat_pow, sym_mat_rec
 
 
-def gradcheck_fn(func, x):
+def gradcheck_fn(func, x, *args):
     """
     Helper function to perform gradcheck on a given function with input x.
     """
     # Batched input testing
-    assert gradcheck(func, [x])
+    assert gradcheck(func, [x, *args])
     # Single input testing
-    assert gradcheck(func, [x[0]])
+    assert gradcheck(func, [x[0], *args])
+
+
+def test_bilinear(x, w):
+    """
+    Test the bilinear function using gradcheck.
+    """
+    gradcheck_fn(bilinear, x, w)
 
 
 def test_sym_mat_log(x):
@@ -26,6 +33,8 @@ def test_sym_mat_exp(x):
     """
     Test the sym_mat_exp function using gradcheck.
     """
+    # Ensure stable input for exp
+    x = sym_mat_log(x)
     gradcheck_fn(sym_mat_exp, x)
 
 
